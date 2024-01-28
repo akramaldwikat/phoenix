dd96a50daecae15d60bf23dc358b89fb73f59f2f
akramaldwikat <76794974+akramaldwikat@users.noreply.github.com>
2024-01-28 14:37:18 +0300

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,126 @@
+$(".slider #slider").slick({
+    slidesToShow: 1,
+    infinite: false,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    variableWidth: true,
+    responsive: [
+        {
+            breakpoint: 480,
+            settings: {
+                dots: false,
+                arrows: false
+            }
+        }
+    ]
+});
+
+$(".news #newSlider").slick({
+    slidesToShow: 4,
+    infinite: false,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    dots: true,
+    // arrows: false, Boolean
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                // rows: 2,
+                infinite: false,
+                slidesToScroll: 1,
+                autoplay: true,
+                autoplaySpeed: 3000,
+                dots: true,
+
+            }
+        },
+        {
+            breakpoint: 800,
+            settings: {
+                slidesToShow: 2,
+                // rows: 2,
+                infinite: false,
+                slidesToScroll: 1,
+                autoplay: true,
+                autoplaySpeed: 3000,
+                dots: true,
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                // rows: 2,
+                infinite: false,
+                slidesToScroll: 1,
+                autoplay: true,
+                autoplaySpeed: 3000,
+                dots: false,
+                arrows: false
+            }
+        }
+    ]
+});
+
+$(".ourMenu #ourMenuSlider").slick({
+    slidesToShow: 4,
+    infinite: false,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    dots: true,
+    // arrows: false, Boolean
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                // rows: 2,
+                infinite: false,
+                slidesToScroll: 1,
+                autoplay: true,
+                autoplaySpeed: 3000,
+                dots: true,
+
+            }
+        },
+        {
+            breakpoint: 800,
+            settings: {
+                slidesToShow: 2,
+                // rows: 2,
+                infinite: false,
+                slidesToScroll: 1,
+                autoplay: true,
+                autoplaySpeed: 3000,
+                dots: true,
+
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                // rows: 2,
+                infinite: false,
+                slidesToScroll: 1,
+                autoplay: true,
+                autoplaySpeed: 3000,
+                dots: false,
+                arrows: false
+            }
+        }
+    ]
+});
+
+$(".Search").click(function () {
+    $(".searchForm").fadeIn();
+});
+
+$(".searchForm .closed").click(function () {
+    $(".searchForm").fadeOut();
+});
