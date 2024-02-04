991299007a5c1982d78c066496b3187100aaad27
akramaldwikat <76794974+akramaldwikat@users.noreply.github.com>
2024-02-04 14:41:12 +0300

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -4,7 +4,6 @@ $(".slider #slider").slick({
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 3000,
-    variableWidth: true,
     responsive: [
         {
             breakpoint: 480,
